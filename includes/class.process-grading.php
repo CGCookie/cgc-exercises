5c844b225705041588355e9995389679765c12d5
Bearded Avenger <email@nickhaskins.com>
2014-11-24 11:59:03 -0600

    comments

--- includes/class.process-grading.php
+++ includes/class.process-grading.php
@@ -26,9 +26,17 @@ class cgc_exercises_process_grading {
 		$value = isset( $_POST['fields'] ) ? $_POST['fields'] : null;
 
 		if ( 'vote=yes' == $value ) {
-			echo 'yes';
+
+			// the vote is yes so let's save some post meta to this submission
+			// need a way to check for previous post meta so this likely needs to be a function
+
+			// need to set a flag for this user that they've already voted on this
+			echo 'you voted yes';
+
 		} else {
-			echo 'no';
+
+			// ok the user has voted no, so let's just return that message and bail out of the function
+			echo 'you voted no';
 		}
 
 		die(); // ajax
