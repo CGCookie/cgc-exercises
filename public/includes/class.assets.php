ec3c2b78808d3de2cccdb7deed786e1948eb6b0d
Bearded Avenger <email@nickhaskins.com>
2015-04-27 11:11:54 -0500

    more clean

--- public/includes/class.assets.php
+++ public/includes/class.assets.php
@@ -10,8 +10,8 @@ class cgc_exercises_assets {
 
 	    if ( 'exercise' == get_post_type() || 'exercise_submission' == get_post_type() ):
 
-	    	wp_enqueue_script('jquery-form');
-			wp_enqueue_style('cgc-exercises-style', CGC_EXERCISES_URL.'/public/assets/css/style.css', CGC_EXERCISES_VERSION, true);
+	    	//wp_enqueue_script('jquery-form');
+			//wp_enqueue_style('cgc-exercises-style', CGC_EXERCISES_URL.'/public/assets/css/style.css', CGC_EXERCISES_VERSION, true);
 			wp_enqueue_script('cgc-exercises', CGC_EXERCISES_URL.'/public/assets/js/cgc-exercises.js', array('jquery'), CGC_EXERCISES_VERSION, true);
 
 			wp_localize_script('cgc-exercises', 'cgc_exercise_meta', array(
