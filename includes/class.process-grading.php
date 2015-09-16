e114cb6bbe2687639901f893685498f63359df62
Bearded Avenger <email@nickhaskins.com>
2015-09-16 16:23:38 -0400

    only reward and mail xp when total votes is equal to the new threshold, 10 by default
    https://github.com/CGCookie/cgcookie-5.0/issues/531

--- includes/class.process-grading.php
+++ includes/class.process-grading.php
@@ -132,6 +132,8 @@ class cgc_exercises_process_grading {
 		// total votes required to pass
 		$vote_allowed     	= get_post_meta( $connected, '_cgc_edu_exercise_passing', true );
 
+		$threshold = get_post_meta( $connected, '_cgc_edu_exercise_vote_threshold', true ) ? get_post_meta( $connected, '_cgc_edu_exercise_vote_threshold', true ) : 10;
+
 		// get submission author
 		$submission_author = get_post_field( 'post_author', $postid );
 		$author_data 		= get_userdata( $submission_author );
@@ -149,7 +151,7 @@ class cgc_exercises_process_grading {
 		update_user_meta( $userid, '_cgc_edu_exercise-'.$postid.'_has_voted', true );
 
 		// 2. if the total # of votes is more than votes allowed
-		if ( $total_votes >= $vote_allowed ) {
+		if ( absint( $total_votes ) == absint( $threshold ) ) {
 
 			// 3. if total yes votes are also more than votes allowed
 			if ( $votes >= $vote_allowed ) {
